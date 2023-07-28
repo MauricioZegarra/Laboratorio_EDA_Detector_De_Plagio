a297f0d8327cc5c25e2971fadc6b6d2522a10831
Mauricio Eduardo Zegarra Puma <mzegarrapu@unsa.edu.pe>
2023-07-28 12:57:08 -0500

    Clase Trie implementada

---
+++ Trie.java
@@ -0,0 +1,38 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            if (Character.isLetter(ch)) { // Verificar si el carácter es una letra
+                char lowerCaseCh = Character.toLowerCase(ch);
+                if (!node.contains(lowerCaseCh)) {
+                    node.put(lowerCaseCh, new TrieNode());
+                }
+                node = node.get(lowerCaseCh);
+            }
+        }
+        node.setEndOfWord(true);
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            if (Character.isLetter(ch)) { // Verificar si el carácter es una letra
+                char lowerCaseCh = Character.toLowerCase(ch);
+                if (!node.contains(lowerCaseCh)) {
+                    return false;
+                }
+                node = node.get(lowerCaseCh);
+            }
+        }
+        return node.isEndOfWord();
+    }
+}
