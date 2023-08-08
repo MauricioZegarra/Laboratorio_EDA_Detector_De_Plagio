01a08392831eb8f16c2e51846eb24bc748c5a3f6
Mauricio Eduardo Zegarra Puma <mzegarrapu@unsa.edu.pe>
2023-08-08 11:23:06 -0500

    Recta Final

--- Main.java
+++ Main.java
@@ -1,22 +1,7 @@
-import java.io.IOException;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         PlagioDetector detector = new PlagioDetector();
-        /*try {
-            List<String> fileContents = FileManager.readAllFilesFromDirectory("archivos/");
-            for (String content : fileContents) {
-                detector.addTextToTrie(content);
-            }
-        } catch (IOException e) {
-            System.out.println("Error al leer los archivos: " + e.getMessage());
-            return;
-        }
-
-        String suspiciousText = "Hay muchos lenguajes en el campo de la programación, pero algunos son verdaderamente icónicos. Java, por ejemplo, ha dejado una huella duradera desde que apareció en los años 90. Actualmente, es uno de los lenguajes más populares y ampliamente adoptados. Parte de este logro se debe a la JVM, que permite que el código Java funcione en diferentes plataformas sin cambios. También es importante mencionar que Java es un lenguaje que prioriza la orientación a objetos, facilitando el desarrollo de soluciones modulares.\n" + //
-                "";
-        boolean isPlagiarized = detector.detectPlagio(suspiciousText);
-        System.out.println(isPlagiarized ? "Plagio detectado." : "No se detectó plagio.");*/
+        
+        detector.setVisible(true);
     }
 }
--- PlagioDetector.java
+++ PlagioDetector.java
@@ -1,12 +1,17 @@
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -14,11 +19,12 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 public class PlagioDetector extends JFrame {
-    private final Trie trie;
+
+    private Trie trie;
     private final EmptyBorder border = new EmptyBorder(10, 10, 10, 10);
+    private String suspicius;
 
     public PlagioDetector() {
-        this.trie = new Trie();
         build();
     }
 
@@ -31,8 +37,7 @@ public class PlagioDetector extends JFrame {
     }
 
     private List<String> tokenize(String text) {
-        // Esto es una simplificación. En un caso real, podrías considerar un
-        // tokenizador más robusto.
+        // Esto es una simplificación. En un caso real, podrías considerar un tokenizador más robusto.
         String[] tokens = text.split("\\s+");
         return new ArrayList<>(Arrays.asList(tokens));
     }
@@ -75,7 +80,7 @@ public class PlagioDetector extends JFrame {
 
         title.setFont(new Font("Verdana", Font.BOLD, 15));
 
-        JButton analyze = new JButton("Subir archivo");
+        JButton analyze = new JButton("Analizar archivo");
         JButton toBD = new JButton("Subir archivos a la BD");
 
         title.setBorder(border);
@@ -91,7 +96,6 @@ public class PlagioDetector extends JFrame {
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 
         setResizable(false);
-        setVisible(true);
 
         this.addWindowListener(new WindowAdapter() {
             @Override
@@ -99,14 +103,54 @@ public class PlagioDetector extends JFrame {
                 close();
             }
         });
+
+        analyze.addActionListener((ActionEvent e) -> {
+            // Permite subir un archivo del ordenador y lo almacena en suspicius
+            JFileChooser fileChooser = new JFileChooser();
+            int returnValue = fileChooser.showOpenDialog(null);
+            
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+                File selectedFile = fileChooser.getSelectedFile();
+                
+                try {
+                    byte[] fileContent = Files.readAllBytes(selectedFile.toPath());
+                    
+                    suspicius = new String(fileContent);
+                    
+                    JOptionPane.showConfirmDialog(null,"Archivo subido exitosamente.");
+                    
+                    verifyPlagiarism();
+                }
+                catch (IOException io) {
+                    JOptionPane.showConfirmDialog(null,"Error al leer el archivo");
+                }
+            } else {
+                System.out.println("No se seleccionó ningún archivo.");
+            }
+        });
     }
 
     private void close() {
-        int confirmation = JOptionPane.showConfirmDialog(this, "¿Está seguro de querer cerrar la aplicación?",
-                "Advertencia",
+        int confirmation = JOptionPane.showConfirmDialog(this, "¿Está seguro de querer cerrar la aplicación?", "Advertencia",
                 JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
         if (confirmation == JOptionPane.YES_OPTION) {
             System.exit(0);
         }
+    }
+    
+    public ResultChecker verifyPlagiarism() throws IOException {
+        
+        ResultChecker result = new ResultChecker(FileManager.readAllFilesFromDirectory("BD/").size());
+        
+        int i = 0;
+        
+        for(String original : FileManager.readAllFilesFromDirectory("BD/")) {
+            this.trie = new Trie();
+            this.addTextToTrie(original);
+            result.setResult(i, this.detectPlagio(suspicius));
+            i++;
+        }
+        
+        return result;
     }
 }
