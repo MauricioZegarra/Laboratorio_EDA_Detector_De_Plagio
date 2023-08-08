9b17b47fb6c7cd9d5b27efbdf239cef337e14e4b
Andre Delgado <adelgadoal@unsa.edu.pe>
2023-08-08 13:44:45 -0500

    para mi

--- fallido/Main.java
+++
@@ -1,8 +0,0 @@
-package fallido;
-public class Main {
-    public static void main(String[] args) {
-        PlagioDetector detector = new PlagioDetector();
-        
-        detector.setVisible(true);
-    }
-}
