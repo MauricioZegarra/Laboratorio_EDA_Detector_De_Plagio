df79ec2d62a3782cd876f5de57dfa88698e4e6da
andre98652 <132710386+andre98652@users.noreply.github.com>
2023-08-08 14:25:47 -0500

    semifinal

--- plagio/FileManager.java
+++ plagio/FileManager.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,5 +20,25 @@ public class FileManager {
             }
         }
         return contents;
+    }
+
+    public static void writeToFile(String filePath, String content) throws IOException {
+        Files.write(Paths.get(filePath), content.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public static List<String> listAllFilesFromDirectory(String path) {
+        List<String> fileNames = new ArrayList<>();
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+    
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                fileNames.add(file.getName());
+            }
+        }
+        return fileNames;
+    }
+    public static String readFile(String filePath) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
     }
 }
--- plagio/Main.java
+++ plagio/Main.java
@@ -1,9 +1,14 @@
 import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+
+    private static PlagioDetector detector;
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        PlagioDetector detector = new PlagioDetector();
+        detector = new PlagioDetector();
 
         try {
             List<String> fileContents = FileManager.readAllFilesFromDirectory("archivos/");
@@ -15,9 +20,86 @@ public class Main {
             return;
         }
 
-        String suspiciousText = "Hay muchos lenguajes en el campo de la programación, pero algunos son verdaderamente icónicos. Java, por ejemplo, ha dejado una huella duradera desde que apareció en los años 90. Actualmente, es uno de los lenguajes más populares y ampliamente adoptados. Parte de este logro se debe a la JVM, que permite que el código Java funcione en diferentes plataformas sin cambios. También es importante mencionar que Java es un lenguaje que prioriza la orientación a objetos, facilitando el desarrollo de soluciones modulares.\n" + //
-                "";
+        boolean running = true;
+        while (running) {
+            showMenu();
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();  // Consume newline
+
+            switch (choice) {
+                case 1:
+                    detectPlagio();
+                    break;
+                case 2:
+                    addNewOriginalText();
+                    break;
+                case 3:
+                    showFileContents();
+                    break;
+                case 4:
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intenta nuevamente.");
+            }
+        }
+    }
+
+    private static void showMenu() {
+        System.out.println("\n--- Menú ---");
+        System.out.println("1. Detectar plagio.");
+        System.out.println("2. Agregar nuevo párrafo original.");
+        System.out.println("3. Ver textos.");
+        System.out.println("4. Salir.");
+        System.out.print("Elige una opción: ");
+    }
+
+    private static void detectPlagio() {
+        System.out.println("\nIntroduce el párrafo sospechoso:");
+        String suspiciousText = scanner.nextLine();
+
         boolean isPlagiarized = detector.detectPlagio(suspiciousText);
         System.out.println(isPlagiarized ? "Plagio detectado." : "No se detectó plagio.");
+    }
+
+    private static void addNewOriginalText() {
+        System.out.println("\nIntroduce el nuevo párrafo original:");
+        String originalText = scanner.nextLine();
+
+        System.out.println("Introduce un nombre para el archivo (sin extensión):");
+        String fileName = scanner.nextLine();
+        
+        try {
+            FileManager.writeToFile("archivos/" + fileName + ".txt", originalText);
+            detector.addTextToTrie(originalText);
+            System.out.println("Párrafo original guardado exitosamente.");
+        } catch (IOException e) {
+            System.out.println("Error al guardar el párrafo: " + e.getMessage());
+        }
+    }
+
+    private static void showFileContents() {
+        try {
+            List<String> files = FileManager.listAllFilesFromDirectory("archivos/");
+            System.out.println("\n--- Lista de archivos ---");
+            for (int i = 0; i < files.size(); i++) {
+                System.out.println((i + 1) + ". " + files.get(i));
+            }
+            System.out.println("Elige un archivo para ver su contenido (por número):");
+
+            int fileChoice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            if (fileChoice > 0 && fileChoice <= files.size()) {
+                String content = FileManager.readFile("archivos/" + files.get(fileChoice - 1));
+                System.out.println("\nContenido del archivo:");
+                System.out.println(content);
+            } else {
+                System.out.println("Selección no válida.");
+            }
+        } catch (IOException e) {
+            System.out.println("Error al listar o leer los archivos: " + e.getMessage());
+        }
     }
 }
