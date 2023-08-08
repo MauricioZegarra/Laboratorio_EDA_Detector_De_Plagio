e5c8938d684a795ebe25fd284ee78a47c579d133
andre98652 <132710386+andre98652@users.noreply.github.com>
2023-08-08 13:52:09 -0500

    avance

---
+++ plagio/TrieNode.java
@@ -0,0 +1,7 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TrieNode {
+    public Map<Character, TrieNode> children = new HashMap<>();
+    public boolean endOfWord = false;
+}
